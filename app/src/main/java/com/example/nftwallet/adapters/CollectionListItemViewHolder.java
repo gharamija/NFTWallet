591c011f1f99a43e2150a1e1d0258a65d48c163c
vlahovivan <ivan.vlahov@gmail.com>
2022-05-21 14:52:50 +0200

    jos stvari

---
+++ app/src/main/java/com/example/nftwallet/adapters/CollectionListItemViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.nftwallet.adapters;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.view.menu.MenuView;
+
+public class CollectionListItemViewHolder {
+    private View itemView;
+    private TextView nameTextView = itemView.findViewById(R.id.)
+
+    public CollectionListItemViewHolder(View itemView) {
+        this.itemView = itemView;
+    }
+
+    public void bind(TempCollection) {
+
+    }
+}
---
+++ app/src/main/java/com/example/nftwallet/adapters/TempCollection.java
@@ -0,0 +1,11 @@
+package com.example.nftwallet.adapters;
+
+public class TempCollection {
+    public String name;
+    public int size;
+
+    public TempCollection(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+}
