e94551d984e82b9ee90d643733671fb015b48c31
Gašpar Haramija <gaspar.haramija@gmail.com>
2022-05-21 10:39:44 +0200

    gaspar1

--- app/src/main/java/com/example/nftwallet/MainActivity.java
+++ app/src/main/java/com/example/nftwallet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nftwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.Visibility;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -25,25 +26,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getData = findViewById(R.id.getData);
-        getData.setOnClickListener(new View.OnClickListener() {
+        final Model[] oneETHtoDollars = {new Model()};
+        Model dollars = new Model();
+
+        Call<Model> modelCall = RetrofitClient.getInstance().getMyApi().getData();
+
+        modelCall.enqueue(new Callback<Model>() {
             @Override
-            public void onClick(View v) {
-                Call<Model> modelCall = RetrofitClient.getInstance().getMyApi().getData();
-
-                modelCall.enqueue(new Callback<Model>() {
-
-                    @Override
-                    public void onResponse(Call<Model> call, Response<Model> response) {
-                        Log.e(TAG, "on response code: "+response.code());
-                        Log.e(TAG, "onResponse: "+ response.body().getDollars());
-                    }
-
-                    @Override
-                    public void onFailure(Call<Model> call, Throwable t) {
-                        Log.e(TAG, "onFailure: "+ t.getMessage());
-                    }
-                });
+            public void onResponse(Call<Model> call, Response<Model> response) {
+                oneETHtoDollars[0] = response.body();
+                Log.e(TAG, "on response code: "+response.code());
+                dollars.setUSD(response.body().getDollars());
+                Log.e(TAG, "onResponse: " + response.body().getDollars());
+            }
+            @Override
+            public void onFailure(Call<Model> call, Throwable t) {
+                Log.e(TAG, "onFailure: "+ t.getMessage());
             }
         });
+
+        System.out.println("Eth to dollars: " + oneETHtoDollars[0].getDollars());
+        System.out.println("drugi pokusaj: " + dollars.getDollars());
+//        getData.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Call<Model> modelCall = RetrofitClient.getInstance().getMyApi().getData();
+//
+//                modelCall.enqueue(new Callback<Model>() {
+//
+//                    @Override
+//                    public void onResponse(Call<Model> call, Response<Model> response) {
+//                        Log.e(TAG, "on response code: "+response.code());
+//                        Log.e(TAG, "onResponse: "+ response.body().getDollars());
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<Model> call, Throwable t) {
+//                        Log.e(TAG, "onFailure: "+ t.getMessage());
+//                    }
+//                });
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/nftwallet/data/Model.java
+++ app/src/main/java/com/example/nftwallet/data/Model.java
@@ -3,6 +3,18 @@ package com.example.nftwallet.data;
 public class Model {
     private String USD;
 
+    public Model() {
+        USD = null;
+    }
+
+    public Model(String USD) {
+        this.USD = USD;
+    }
+
+    public void setUSD(String USD) {
+        this.USD = USD;
+    }
+
     public String getDollars() {
         return USD;
     }
