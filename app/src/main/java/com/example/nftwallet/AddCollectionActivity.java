2c507f01659cfbe8e7d585afcc8ac9071a2ab0b0
Gašpar Haramija <gaspar.haramija@gmail.com>
2022-05-21 19:58:29 +0200

    make new collection feature

--- app/src/main/java/com/example/nftwallet/AddCollectionActivity.java
+++ app/src/main/java/com/example/nftwallet/AddCollectionActivity.java
@@ -2,13 +2,43 @@ package com.example.nftwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class AddCollectionActivity extends AppCompatActivity {
+import com.example.nftwallet.database.CollectionDao;
+import com.example.nftwallet.database.Entities.Collection;
+import com.example.nftwallet.database.NFTWalletDatabase;
+
+public class AddCollectionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String textInput;
+    private NFTWalletDatabase database;
+    private Button button;
+    private TextView writtenText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_collection);
+        button = findViewById(R.id.save_button);
+        writtenText = findViewById(R.id.input_text);
+        button.setOnClickListener(this);
+
+    }
+
+    private void setupDatabase(){
+        database = NFTWalletDatabase.getInstance(this.getApplicationContext());
+        database.collectionDao().insertCollection(new Collection(this.textInput));
+    }
+
+    @Override
+    public void onClick(View v) {
+        textInput = writtenText.getText().toString();
+        setupDatabase();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
